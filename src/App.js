86ab939808becece0593982bcecc9c45098b7115
fatihozkul35 <fatihozkul3541@gmail.com>
2022-06-08 10:17:18 +0200

    router created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import About from "./Pages/About/About";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
@@ -10,9 +13,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/home" element={<Home />} />
-          {/* <Route exact path="/about" element={<About />} />
+          <Route exact path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
