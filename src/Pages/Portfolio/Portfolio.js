67e46ca6dd6dae99b14c01aa65fbc306ca931b47
fatihozkul35 <fatihozkul3541@gmail.com>
2022-06-11 16:19:19 +0200

    images and links uploaded

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -2,13 +2,50 @@ import React from 'react'
 import "./Portfolio.css";
 import Navbar from "../../Components/Navbar/Navbar";
 
-// const projectsOfData = [
-//   {
-//     link: "/#",
-//     img: "https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-//     title:"Blog App"
-//   },
-// ];
+const projectsOfData = [
+  {
+    link: "https://reacttodocreatinbyadamsandler.netlify.app/",
+    img: "/static/images/ToDoAPP-react.png",
+    title: "To Do App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+  {
+    link: "https://javascript-restaurant-menu.netlify.app/",
+    img: "/static/images/JavaScript-Restaurant-Menü.png",
+    title: "Restaurant Menü App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+  {
+    link: "https://008calculator.netlify.app/",
+    img: "/static/images/JavaScriptCalculator.png",
+    title: "Calculator App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+  {
+    link: "https://reactcapstoneproject.netlify.app/",
+    img: "/static/images/react-capstone.png",
+    title: "To Do App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+  {
+    link: "https://oclockproject.netlify.app/",
+    img: "/static/images/react-Oclock.png",
+    title: "Oclock App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+  {
+    link: "https://react-tourplaces.netlify.app/",
+    img: "/static/images/React-Tour-Places.png",
+    title: "To Do App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+  {
+    link: "https://recipe-app-api-axios.netlify.app/",
+    img: "/static/images/recipe-app-API.png",
+    title: "To Do App",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.`,
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -17,92 +54,25 @@ const Portfolio = () => {
       {/* div wrapper */}
       <div className="sub-section">
         {/* div card */}
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
+        {projectsOfData.map((data, i) => {
+          console.log(data)
+          const {link, img, title, description} = data;
+          return (
+            <div key={i} className="img-project">
+              <img src={img} alt="here is a pic of any project" />
+              {/* div info */}
+              <div className="project-info">
+                <h1 className="title">{title}</h1>
+                <p>{description}</p>
+                <a href={link} target="_blank" rel="noreferrer" className="btn">
+                  Go to Project
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
-            </div>
-          </div>
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
-            </div>
-          </div>
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
-            </div>
-          </div>
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
-            </div>
-          </div>
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
-            </div>
-          </div>
-          <div className="img-project">
-            <img
-              src="https://images.unsplash.com/photo-1635967200576-004b9c9e7fb7?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt="here is a pic of any project"
-            />
-            {/* div info */}
-            <div className="project-info">
-              <h1 className="title">Title</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, hic.</p>
-              <a href="/#" className="btn">Go to Project</a>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
+    </div>
   );
 }
 
